38b8042d2f8a18d5edfc8dbc2bb78fcbdb253d89
Cássio Estevão <116197477+cassioestevao@users.noreply.github.com>
2024-03-02 15:57:01 -0300

    Add files via upload

---
+++ chatpython.py
@@ -0,0 +1,56 @@
+import openai
+import tkinter as tk
+from tkinter import PhotoImage
+
+
+openai.api_key = "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"
+
+
+root = tk.Tk()
+root.title("ChatAssist | Cássio Estevão")
+root.config(bg="white")
+root.resizable(False, False)
+
+
+def enviar_mensagem(mensagem):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Você está conversando com o chat GPT indiretamente!"},
+            {"role": "user", "content": mensagem},
+        ],
+    )
+    return response["choices"][0]["message"]["content"]
+
+def enviar_mensagem_gui():
+    mensagem = entry.get()
+    response = enviar_mensagem(mensagem)
+    chat_history.config(state=tk.NORMAL)
+    chat_history.insert(tk.END, f"Você: {mensagem}\n")
+    chat_history.insert(tk.END, f"Assistente: {response}\n\n")
+    chat_history.config(state=tk.DISABLED)
+    entry.delete(0, tk.END)
+
+chat_history = tk.Text(root, 
+                       height=20, 
+                       width=70, 
+                       state=tk.DISABLED
+                       )
+entry = tk.Entry(root, 
+                 width=80
+                 )
+send_button = tk.Button(root, 
+                        text="Enviar", 
+                        command=enviar_mensagem_gui,
+                        bg="white"
+                        )
+
+python_logo = PhotoImage(file="python_logo.png",height=50)
+logo_label = tk.Label(root, image=python_logo)
+logo_label.pack()
+
+chat_history.pack()
+entry.pack()
+send_button.pack()
+
+tk.mainloop()
